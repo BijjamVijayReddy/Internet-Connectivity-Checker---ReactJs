d0b4126866b5388306bd2afbd8b1fb00c15a5527
Bijjamvijayreddy <bijjamvijayreddy@gmail.com>
2023-08-25 12:13:17 +0530

    minor issue is fixed

--- src/App.js
+++ src/App.js
@@ -51,14 +51,14 @@ const App = () => {
         horizontal: 'right'
       }} onClose={handleClose}>
         <Alert variant="filled" severity="success" onClose={handleClose}>
-        "Great! You're now connected to the internet."
+        Great! You're now connected to the internet.
         </Alert>
       </Snackbar>) : (<Snackbar open={isOpen} sx={{ maxWidth: "90%", margin: "0 auto" }} autoHideDuration={4000} anchorOrigin={{
         vertical: 'top',
         horizontal: 'right'
       }} onClose={handleClose}>
         <Alert variant="filled" severity="error" onClose={handleClose}>
-          "Looks like you're not connected to the internet. Please verify your connection."
+          Looks like you're not connected to the internet. Please verify your connection.
          </Alert>
       </Snackbar>)}
 
